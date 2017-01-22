3d154a4ba29be0026a221a18fa8b776431f26b88
dwanmight <dwanmight@gmail.com>
2017-01-22 22:37:32 +0200

    Initial commit

---
+++ app/src/main/java/com/junior/dwan/remotecontrol/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.junior.dwan.remotecontrol;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends FragmentActivity  {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
+
+    protected  int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+
+
+}
